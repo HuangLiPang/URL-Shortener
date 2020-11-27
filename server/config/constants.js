296eb2af540975659a64c130c9c4800ae78ce882
huanglipang <lh5jv@virginia.edu>
2020-11-26 23:53:13 -0500

    rm server config

--- server/config/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-        "mongodb+srv://huanglipang:huanglipang@cluster0.3jp43.mongodb.net/url-shortener?retryWrites=true&w=majority",
-    baseUrl: "http://localhost:2053",
-};

185cbf3824f4d165868f20dd183456ac10f95600
huanglipang <lh5jv@virginia.edu>
2020-11-27 14:51:09 -0500

    update settings

--- client/src/APIHelper.js
+++ client/src/APIHelper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import constants from "./config/constants";
-axios.defaults.baseURL = constants.apiUrl;
+// axios.defaults.baseURL = constants.apiUrl;
+axios.defaults.baseURL = process.env.BASE_URL + "api/";
 
 export const createShortUrl = (obj) => {
     const requestUrl = "shorten";
